9621d52d97a21574add245ae6d690d9479ac84b1
Fabio Mariano <fgmariano@outlook.com.br>
2019-03-05 10:14:57 -0300

    First commit

---
+++ .config/i3/lock.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TMPBG=/tmp/screen.png
+LOCK=$HOME/.config/i3/lock.png
+RES=1366x768
+
+ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG
+i3lock -i $TMPBG
+rm $TMPBG
